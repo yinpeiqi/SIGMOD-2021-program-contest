5a3635bb8db7aa8e5dd317fe7ddd48399a84a8a2
yinpe <11810305@mail.sustech.edu.cn>
2021-04-12 22:09:51 +0800

    feat: update group by

--- lltql.py
+++ lltql.py
@@ -1,3 +1,5 @@
+import math
+
 import pandas as pd
 
 website2id = {}
@@ -13,8 +15,6 @@ def generate(data, name):
         for j in range(i+1, length):
             temp = []
             temp.append(true_table[website2id[array[i][0]]][website2id[array[j][0]]])
-            temp.append(website2id[array[i][0]])
-            temp.append(website2id[array[j][0]])
             for t in range(1, len(array[i])):
                 temp.append(array[i][t])
                 temp.append(array[j][t])
@@ -22,20 +22,38 @@ def generate(data, name):
 
     df = pd.DataFrame(result)
     df.rename(columns={0: 'label'}, inplace=True)
-    col_len = len(columns)
-    for i in range(col_len):
-        df.rename(columns={((i+1)*2-1): 'left_'+columns[i], ((i+1)*2): 'right_'+columns[i]}, inplace=True)
+    # col_len = len(columns)
+    # for i in range(col_len):
+    #     df.rename(columns={((i+1)*2+1): 'left_'+columns[i], ((i+1)*2): 'right_'+columns[i]}, inplace=True)
+    df.rename(columns={1: 'left_title', 2: 'right_title'}, inplace=True)
     df.index.name = 'id'
-
     df.to_csv(name+".csv", sep=',', encoding='utf-8')
 
 
 if __name__ == '__main__':
-    Xdata = pd.read_csv('./test/cleanX2.csv')
-    Ydata = pd.read_csv('./data/Y2.csv')
-
+    dataset = '4'
+    Xdata = pd.read_csv('X' + dataset + '.csv')
     Xarray = Xdata.sample(frac=1).values.tolist()
+    Xtemp = []
+    for line in Xarray:
+        temp = []
+        temp.append(line[-1])
+        mess = line[0]
+        for i in range(1, len(line)-1):
+            if line[i] is not None and (type(line[i]) != float or not math.isnan(line[i])) and str(line[i]) not in mess:
+                mess = mess + ' - ' + str(line[i])
+        temp.append(mess)
+        Xtemp.append(temp)
+    Xarray = Xtemp
+    Xdata = pd.DataFrame(Xarray)
+    Xdata.rename(columns={0: 'instance_id', 1: 'title'}, inplace=True)
+    # if dataset == '4':
+    #     Xdata = Xdata.filter(['instance_id', 'name'], axis=1)
+    # else:
+    #     Xdata = Xdata.filter(['instance_id', 'title'], axis=1)
+    Ydata = pd.read_csv('Y' + dataset + '.csv')
 
+    print(Xdata)
     tot_id = 0
     for line in Xarray:
         tot_id += 1
@@ -55,8 +73,8 @@ if __name__ == '__main__':
         true_table[rid][lid] = label
 
 
-    train_data = Xdata.sample(frac=0.6, axis=0)
-    test_data = Xdata[~Xdata.index.isin(train_data.index)]
+    train_data = Xdata.sample(frac=1, axis=0)
+    # test_data = Xdata[~Xdata.index.isin(train_data.index)]
 
-    generate(train_data, 'train')
-    generate(test_data, 'test')
+    generate(train_data, 'train' + dataset)
+    # generate(test_data, 'test' + dataset)
