7c3ef5620cdbdce7846495247add188d931762a8
yinpe <11810305@mail.sustech.edu.cn>
2021-04-15 12:21:36 +0800

    feat: add change

--- clean_x4.py
+++ clean_x4.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import re
 
-brand_list = ["intenso", "lexar", "sony", "pny", "sandisk", "kingston", "samsung", "toshiba", "transcend"]
+brand_list = ["intenso", "pny", "lexar", "sony", "sandisk", "kingston", "samsung", "toshiba", "transcend"]
 
 intenso_type = ["basic", "premium", "rainbow", "speed"]
+
 
 def clean_x4(Xdata):
     names = Xdata.filter(items=['name'], axis=1).fillna('')
@@ -24,6 +25,7 @@ def clean_x4(Xdata):
 
         size = '0'
         price = '0'
+        mem_type = '0'
         brand = '0'
         type = '0'
         model = '0'
@@ -31,7 +33,7 @@ def clean_x4(Xdata):
         if sizes[row][0] != '':
             size = sizes[row][0].lower().replace(' ', '')
         else:
-            size_model = re.search(r'[0-9]{1,4}[ ]*[g][b]', nameinfo)
+            size_model = re.search(r'[0-9]{1,4}[ ]*[gt][bo]', nameinfo)
             if size_model is not None:
                 size = size_model.group()[:].lower()
 
@@ -45,6 +47,24 @@ def clean_x4(Xdata):
                 if b in nameinfo:
                     brand = b
                     break
+
+        mem_model = re.search(r'ssd', nameinfo)
+        if mem_model is None:
+            mem_model = re.search(r'micro[- ]?sd[hx]?c?', nameinfo)
+        if mem_model is None:
+            mem_model = re.search(r'sd[hx]c', nameinfo)
+        if mem_model is None:
+            mem_model = re.search(r'usb', nameinfo)
+        if mem_model is None:
+            mem_model = re.search(r'sd', nameinfo)
+        if mem_model is not None:
+            mem_type = mem_model.group()
+            if mem_type not in ('ssd', 'usb'):
+                if 'micro' in mem_type:
+                    mem_type = 'microsd'
+                else:
+                    mem_type = 'sd'
+
 
         if brand == "intenso":
             model_model = re.search(r'[0-9]{7}', nameinfo)
@@ -60,16 +80,88 @@ def clean_x4(Xdata):
                         type = t
                         break
 
-            print(nameinfo,model,type)
 
         elif brand == "lexar":
-            pass
+            model_model = re.search(r'xqd', nameinfo)
+            if model_model is not None:
+                model = model_model.group()
+
+            type_model = re.search(r'((ljd)|[\s])[a-wy-z][0-9]{2}[a-z]?', nameinfo)
+            if type_model is None:
+                type_model = re.search(r'[\s][0-9]+00x(?![a-z0-9])', nameinfo)
+            if type_model is None:
+                type_model = re.search(r'[\s]x[0-9]+00', nameinfo)
+            if type_model is not None:
+                type = type_model.group().strip().replace('x', '').replace('l', '').replace('j', '').replace('d', '')
+        # judge type and model
 
 
-        # print(nameinfo,brand)
+        elif brand == 'sony':
+            type_model = re.search(r'usm[-]?[0-9]?[0-9]?[0-9]?[a-z]{1,4}', nameinfo)
+            if type_model is not None:
+                type = type_model.group().replace('-', '').replace('g', '')
+                for c in range(ord('0'), ord('9')):
+                    type = type.replace(chr(c), '')
+            else:
+                type_model = re.search(r'((class?e?[\s-]?)|(cl[ -]))[0-9]{1,4}', nameinfo)
+                if type_model is not None:
+                    type = type_model.group().replace('-', '').replace(' ', '')
+                    for c in range(ord('a'), ord('z')):
+                        type = type.replace(chr(c), '')
+                    type = 'class ' + type
+        # 1024: 1 TB
+        # 256: ssd
+        # 128: usmqx usb
+        # 128: microsd
+        # 64: usb
+        # 32: usmqx
+        # 32: sd | microsd
+        # 32: usmr & usb | usb
+        # 16: sd
+        # 16: usb
+        # 8: sd
+        # 8: microsd
+        # 8: usmqx usb
+        # 4: usmr
+        # 4: usmmp | usb
 
-        # print(instance_ids[row][0], brands[row][0], size_model)
-        # result.append([instance_ids[row][0], brands[row][0], size_model])
+        elif brand == 'sandisk':
+            model_model = re.search(r'glide', nameinfo)
+            if model_model is None:
+                model_model = re.search(r'ext', nameinfo)
+            if model_model is None:
+                model_model = re.search(r'ultra', nameinfo)
+            if model_model is None:
+                model_model = re.search(r'cruzer', nameinfo)
+            if model_model is not None:
+                model = model_model.group()
+            print(nameinfo, model)
+        # 256: ext
+        # 256: glide
+        # 128: ext
+        # 128: ultra & microsd
+        # 128: cruzer | usb (else)
+        # 64: ultra & sd
+        # 64: ultra & microsd
+        # 64: ext & usb
+        # 64: ultra & usb
+        # 64: usb
+        # 32: ultra & sd
+        # 32: glide
+        # 32: ultra microsd | ultra sd | ext sd
+        # 16: ultra & sd
+        # 16: microsd
+        # 16: usb
+        # 16: ext sd
+        # 16: ext usb
+        # 16: ext sd ?
+        # 8: sd
+        # 8: ultra & usb
+        # 8: cruzer & usb
+        # 8: ext sd
+        # 4: cruzer
+        # 4: microsd
+
 
     mp = {}
     cnt = 0
