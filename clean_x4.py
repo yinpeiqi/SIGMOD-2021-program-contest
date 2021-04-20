e5be680e57c8ebe05d96c121149eac75ea561bf2
yinpe <11810305@mail.sustech.edu.cn>
2021-04-20 19:29:34 +0800

    feat: modify samsung

--- clean_x4.py
+++ clean_x4.py
@@ -105,11 +105,11 @@ def clean_x4(Xdata):
                 mem_type = 'usb'
             generation = gen_model.group()[-3:].replace('.0', '').replace('.1', '').replace('.2', '')
         else:
-            gen_model = re.search(r'uhs[\\]?[\- ]?[i1]', nameinfo)
+            gen_model = re.search(r'uhs[\\]?[\- ]?[i1](?!i)', nameinfo)
             if gen_model is not None:
                 generation = 1
             if gen_model is None:
-                gen_model = re.search(r'uhs[\\]?[\- ]?[i2][i]?', nameinfo)
+                gen_model = re.search(r'uhs[\\]?[\- ]?(2|(ii))', nameinfo)
                 if gen_model is not None:
                     generation = 2
             if gen_model is not None and mem_type == '0':
@@ -164,13 +164,16 @@ def clean_x4(Xdata):
                 elif 'usm' in nameinfo:
                     mem_type = 'usb'
 
-            type_model = re.search(r'((sf)|(usm))[-]?[0-9a-z]{1,6}', nameinfo)
-            if type_model is not None:
-                type = type_model.group().replace('-', '').replace('g', '')
-                for c in range(ord('0'), ord('9')):
-                    type = type.replace(chr(c), '')
-                if type == 'sfn' and mem_type == '0':
-                    mem_type = 'sd'
+            if size == '1tb':
+                mem_type = 'usb'
+            else:
+                type_model = re.search(r'((sf)|(usm))[-]?[0-9a-z]{1,6}', nameinfo)
+                if type_model is not None:
+                    type = type_model.group().replace('-', '').replace('g', '')
+                    for c in range(ord('0'), ord('9')):
+                        type = type.replace(chr(c), '')
+                    if type == 'sfn' and mem_type == '0':
+                        mem_type = 'sd'
         # 1024: 1 TB
         # 256: ssd
         # 128: usmqx usb
@@ -361,6 +364,10 @@ def clean_x4(Xdata):
                         model = ch + '101'
                     elif speed == '90':
                         model = ch + '302'
+            if mem_type == 'usb' and generation == '2' and model == '0':
+                model = 'u202'
+            if mem_type == 'sd' and generation == '2' and model == '0':
+                model = 'n101'
 
         elif brand == 'transcend':
             pass
